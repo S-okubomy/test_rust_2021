86dcd57d1327d14c42ccc58d9e9830c50377e697
S-okubomy <okubo1504@gmail.com>
2022-02-23 08:43:25 +0000

    update

---
+++ test_rust1/src/at_code1.rs
@@ -0,0 +1,97 @@
+#[allow(dead_code)]
+// fn read<T: std::str::FromStr>() -> T {
+//     let mut s = String::new();
+//     std::io::stdin().read_line(&mut s).ok();
+//     s.trim().parse().ok().unwrap()
+// }
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>().split_whitespace()
+        .map(|e| e.parse().ok().unwrap()).collect()
+}
+
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+
+
+// fn main() {
+//     let num1 = read::<i32>();
+//     println!("read: {}", num1);
+
+//     let num2 = read_vec::<i32>();
+//     println!("read2: {:?}", num2);
+// }
+
+fn read <T> () -> T
+    where T: std::str::FromStr
+{
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn main() {
+    let n: u8 = read::<u8>();
+    let mut a_vec: Vec<u32> = read::<String>().split_whitespace().map(|s| s.parse().ok().unwrap()).collect();
+
+    let mut cnt = 0;
+    'outer: loop {
+        for i in 0..(n as usize) {
+            if a_vec[i]%2 != 0 { 
+                break 'outer;
+            }
+            a_vec[i] /= 2;
+        }
+        cnt += 1;
+    }
+
+    println!("{}", cnt);
+
+
+    // let s_arr = read::<String>();
+    // let mut cnt = 0;
+    // for s in s_arr.chars() {
+    //     match s {
+    //         '1' => cnt += 1,
+    //         _ =>  (),
+    //     }
+    //     // if "1" == s.to_string() {
+    //     //     cnt += 1;
+    //     // }
+    // }
+    // println!("{}", cnt);
+
+    // let vec :Vec<i32> = read::<String>().split_whitespace().map(|s| s.parse().ok().unwrap()).collect();
+    // let a = vec[0];
+    // let b = vec[1];
+    // if a * b % 2 != 0 {
+    //     println!("Odd");
+    // } else {
+    //     println!("Even");
+    // }
+
+
+    // let a: i32 = read::<i32>();
+    // let vec: Vec<i32> = read::<String>().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    // let b = vec[0];
+    // let c = vec[1];
+    // let s = read::<String>();
+    // println!("{} {}", a+b+c, s);
+
+
+    // let mut s = String::new();
+    // std::io::stdin().read_line(&mut s).ok();
+    // let a = s.trim().parse::<i32>().ok().unwrap();
+    // let mut s2 = String::new();
+    // let _ = std::io::stdin().read_line(&mut s2);
+    // let vec: Vec<i32> = s2.split_whitespace().map(|b| b.parse::<i32>().unwrap()).collect();
+    // let b = vec[0];
+    // let c = vec[1];
+    // let mut s3 = String::new();
+    // let _ = std::io::stdin().read_line(&mut s3);
+    // let st = s3;
+    // println!("{} {} {} {}", a, b, c, st);
+}
