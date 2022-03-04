614322a572acc6e42619d998bfce1e1e409025f3
S-okubomy <okubo1504@gmail.com>
2022-03-04 08:39:58 +0000

    update

--- at_cd1/src/main.rs
+++ at_cd1/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     
     T_X_Y.append(&mut pos); // 初期状態
 
-    if canTravel(N, T_X_Y) {
+    if can_travel(N, T_X_Y) {
         println!("Yes");
     } else {
         println!("No");
@@ -18,7 +18,7 @@ fn main() {
 }
 
 #[allow(non_snake_case)]
-fn canTravel(N: usize, T_X_Y: Vec<(i32, i32, i32)>) -> bool {
+fn can_travel(N: usize, T_X_Y: Vec<(i32, i32, i32)>) -> bool {
     for i in 0..N {
         let (ti, xi, yi) = T_X_Y[i];
         let (ti_p1, xi_p1, yi_p1) = T_X_Y[i+1];
@@ -32,7 +32,6 @@ fn canTravel(N: usize, T_X_Y: Vec<(i32, i32, i32)>) -> bool {
 }
 
 
-
 /// テスト用
 /// cargo build && cargo test
 #[cfg(test)]
@@ -40,34 +39,45 @@ mod tests {
     use cli_test_dir::*;
     use std::process::Output;
 
-    const BIN: &'static str = "./at_cd1";
+    const BIN: &'static str = "./at_cd1"; // 実行ファイル名
 
     #[test]
-    fn test1() {
-        // let testdir = TestDir::new(BIN, "");
-        // let output = testdir
-        //     .cmd()
-        //     .output_with_stdin(r#"
-        //     2
-        //     3 1 2
-        //     6 1 1            
-        //     "#)
-        //     .tee_output()
-        //     .expect_success();
-        
+    fn test1() {        
         let input_str: &str = r#"
             2
             3 1 2
             6 1 1                  
         "#;
         let output: Output = output(input_str);
-
         assert_eq!(output.stdout_str(), "Yes\n");
-        assert!(output.stderr_str().is_empty());
+        assert!(output.stderr_str().is_empty()); // 値がtrueならテスト成功
     }
 
     #[test]
     fn test2() {
+        let input_str = r#"
+            1
+            2 100 100
+        "#;
+        let out = output(input_str);
+        assert_eq!(out.stdout_str(), "No\n");
+        assert!(out.stderr_str().is_empty());
+    }
+
+    #[test]
+    fn test3() {
+        let input = r#"
+            2
+            5 1 1
+            100 1 1
+        "#;
+        let out = output(input);
+        assert_eq!(out.stdout_str(), "No\n");
+        assert!(out.stderr_str().is_empty());
+    }
+
+    #[test]
+    fn test_other() {
         assert_eq!(1+2, 3);
     }
 
