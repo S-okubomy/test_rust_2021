746d3b8f84221a6fc781ccf68def068c3a75f1fe
okubo <okubo1504@gmail.com>
2022-03-23 13:50:45 +0000

    update

--- at_cd1/src/atc13.rs
+++ at_cd1/src/atc13.rs
@@ -2,12 +2,68 @@ use proconio::{ input, fastout };
 
 
 fn main() {
-    how_many_guests();
+    travel();
 }
 
+/// https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_ak
+fn travel() {
+    println!("TODO");
+}
+
+/// https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_aj
+#[allow(dead_code)]
+#[allow(non_snake_case)]
+#[fastout]
+fn snowy_days() {
+    input! {
+        N: usize, Q: usize,
+        mut LRX: [(usize, usize, usize); Q],
+    }
+    LRX.insert(0, (0, 0, 0));
+
+    // 階差
+    let mut B: Vec<isize> = vec![0; N+2];
+    for i in 1..=Q {
+        let (L, R, X) = LRX[i];
+        B[L] += X as isize;
+        B[R+1] -= X as isize;
+    }
+
+    for i in 2..=N {
+        if 0 < B[i] {
+            print!("<");
+        } else if 0 == B[i] {
+            print!("=");
+        } else {
+            print!(">");
+        }
+    }
+}
+
+
 /// https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_ai
+#[allow(dead_code)]
 #[allow(non_snake_case)]
 #[fastout]
 fn how_many_guests() {
-    println!("TODO 実装");
+    input! {
+        N: usize, Q: usize,
+        mut A: [usize; N],
+        mut LR: [(usize, usize); Q],
+    }
+
+    A.insert(0, 0);
+    LR.insert(0, (0, 0));
+
+    // 累積和
+    let mut B: Vec<usize> = vec![0; N+1];
+    for i in 1..=N {
+        B[i] = B[i-1] + A[i];
+    }
+    
+    for i in 1..=Q {
+        let (L, R) = LR[i];
+        let ans = B[R] - B[L-1];
+        println!("{}", ans);
+    }
 }
