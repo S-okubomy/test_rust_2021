7f5b8629851a48fd2ed1e8ac2a7022b52c25c441
okubo <okubo1504@gmail.com>
2022-04-03 06:44:10 +0000

    update

---
+++ at_cd1/src/atc_o3.rs
@@ -0,0 +1,9 @@
+fn main() {
+    edge_chk();
+}
+
+/// https://qiita.com/sano192/items/9c32dafea7ce34d67eac
+/// https://atcoder.jp/contests/abc240/tasks/abc240_a
+fn edge_chk() {
+    println!("TODO");
+}
