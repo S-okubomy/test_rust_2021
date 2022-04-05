cac2880ccd26cb9beebeaa5cd47ffd83d86fed03
okubo <okubo1504@gmail.com>
2022-04-05 12:37:43 +0000

    update

--- at_cd1/src/atc_o3.rs
+++ at_cd1/src/atc_o3.rs
@@ -1,14 +1,89 @@
 use proconio::{ input, fastout };
 use std::collections::{ HashSet };
 
+fn yes_or_no(b: bool) -> String {
+    if b {
+        String::from("Yes")
+    } else {
+        String::from("No")
+    }
+}
+
 fn main() {
-    horizon();
+    exponential_or_quadratic();
+}
+
+/// https://qiita.com/sano192/items/abea29fc42030300f379
+fn exponential_or_quadratic() {
+    println!("TODO");
+}
+
+
+/// https://atcoder.jp/contests/abc239/tasks/abc239_c
+#[allow(non_snake_case)]
+// #[fastout]
+#[allow(dead_code)]
+fn knight_fork() {
+    input! {
+        x1: isize, y1: isize, x2: isize, y2: isize, 
+    }
+
+    let start_x = x1 - 2;
+    let end_x = x1 + 2;
+    let start_y = y1 - 2;
+    let end_y = y1 + 2;
+
+    let mut exist: bool = false;
+    'outer: for x in start_x..=end_x {
+        for y in start_y..=end_y {
+            if dist_sq(x1, y1, x , y) == 5 && dist_sq(x2, y2, x, y) == 5 {
+                exist = true;
+                break 'outer;
+            }
+        }
+    }
+
+    println!("{}", yes_or_no(exist));
+}
+
+fn dist_sq(px: isize, py: isize, x: isize, y: isize) -> isize {
+    (px - x).pow(2) + (py - y).pow(2)
+} 
+
+/// https://atcoder.jp/contests/abc239/tasks/abc239_b
+#[allow(non_snake_case)]
+#[fastout]
+#[allow(dead_code)]
+fn integer_division() {
+    input! {
+        X: isize,
+    }
+
+    let mut ans = X / 10;
+    if X < 0 {
+        if X % 10 == 0 {
+            ans = X / 10;
+        } else {
+            ans = X / 10 -1;
+        }
+    }
+
+    println!("{}", ans);
 }
 
 /// https://atcoder.jp/contests/abc239/tasks/abc239_a
 /// https://qiita.com/sano192/items/d7fe37db64d946c07581
+#[allow(non_snake_case)]
+#[fastout]
+#[allow(dead_code)]
 fn horizon() {
-    println!("TODO")
+    input! {
+        H: f64,
+    }
+
+    let ans = (H * (12800000.0 + H)).sqrt();
+
+    println!("{}", ans)
 }
 
 /// https://atcoder.jp/contests/abc240/tasks/abc240_c
@@ -52,13 +127,6 @@ fn jumping_takahashi() {
     */
 }
 
-fn yes_or_no(b: bool) -> String {
-    if b {
-        String::from("Yes")
-    } else {
-        String::from("No")
-    }
-}
 
 
 
