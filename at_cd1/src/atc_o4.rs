7592ad2b3a248e82bd76138ddf8902a93e7fb527
okubo <okubo1504@gmail.com>
2022-04-12 14:06:00 +0000

    update

--- at_cd1/src/atc_o4.rs
+++ at_cd1/src/atc_o4.rs
@@ -1,8 +1,87 @@
+use proconio::{ input, fastout }; 
+
 fn main() {
-    ten_yen_stamp();
+    product2();
+}
+
+fn product2() {
+    // TODO 全探索
+    // https://atcoder.jp/contests/abc233/submissions/28354159
+    println!("TODO");
+}
+
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+#[fastout]
+fn product1() {
+    input! {
+        N: usize, X: usize,
+        A: [[usize]; N], // 先頭が要素数なら要素数省略可
+    }
+    // println!("{:?}", A);
+
+    //  積=1,袋の番号0からスタート
+    let ans = dfs(1, 0, N, X, &A);
+
+    println!("{}", ans);
+}
+
+#[allow(non_snake_case)]
+fn dfs(seki: usize, pos: usize, N: usize, X: usize, A_vec: &Vec<Vec<usize>>) -> usize {
+    let mut ans = 0;
+    if pos == N {
+        if seki == X {
+            ans += 1;
+        }
+        return ans;
+    }
+
+    for ai in &A_vec[pos] {
+        // if seki.saturating_mul(*ai) > X { continue };
+        if seki > X / ai { continue }; // オーバフローしないように
+        ans += dfs(seki * ai, pos + 1, N, X, A_vec);
+    }
+    ans
+}
+
+
+
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+#[fastout]
+fn a_reverse() {
+    input! {
+        L: usize, R: usize,
+        S: String,
+    }
+
+    let mut s_vec: Vec<String> = S.chars().map(|c| { c.to_string() }).collect();
+    let mut l_pos = L - 1;
+    let mut r_pos = R - 1;
+    while l_pos < r_pos {
+        s_vec.swap(l_pos, r_pos);
+        l_pos += 1;
+        r_pos -= 1;
+    }
+    println!("{}", s_vec.join(""));
 }
 
 /// https://qiita.com/sano192/items/e54a9f9b994781709881
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+#[fastout]
 fn ten_yen_stamp() {
-    println!("TODO");
+    input! {
+        X: usize, Y: usize,
+    }
+
+    if X >= Y {
+        println!("0");
+    } else {
+        println!("{}", ceil(Y-X));
+    }
+}
+
+fn ceil(x: usize) -> usize {
+    (x as f64 / 10_f64).ceil() as usize
 }
