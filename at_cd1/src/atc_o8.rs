24566fdea43fbe14d242282d589d5200aac323d7
okubo <okubo1504@gmail.com>
2022-05-13 14:16:50 +0000

    update

--- at_cd1/src/atc_o8.rs
+++ at_cd1/src/atc_o8.rs
@@ -1,7 +1,68 @@
 use proconio::{ input, fastout };
 
 fn main() {
-    ubiquity();
+    staircase_sequences();
+}
+
+#[allow(dead_code)]
+fn staircase_sequences() {
+    input! {
+        mut n: usize,
+    }
+    let mut i = 1;
+    let mut cnt = 0;
+    while i * i <= 2*n {
+        if 2*n % i == 0 {
+            let x = i;
+            let y = 2*n / x;
+            if x % 2 != y % 2 {
+                cnt += 2;
+            }
+        }
+        i += 1;
+    }
+    println!("{}",  cnt);
+}
+
+#[allow(dead_code)]
+fn prediction_and_restriction() {
+    input! {
+        n: usize, k: usize,
+        r: usize, s: usize, p: usize,
+        t: String,
+    }
+    let mut t_vec: Vec<char> = t.chars().collect();
+    for i in k..n {
+        if t_vec[i-k] == t_vec[i] {
+            t_vec[i] = 'x';
+        }
+    }
+
+    let mut sum = 0;
+    for i in 0..n {
+        sum += match t_vec[i] {
+            'r' => p,
+            's' => r,
+            'p' => s,
+            _ => 0,
+        }
+    }
+    println!("{}", sum);
+}
+
+#[allow(dead_code)]
+fn i_hate_factorization() {
+    input! {
+        x: i64,
+    }
+    for a in -1000..=1000 {
+        for b in -1000..=1000 {
+            if (a as i64).pow(5) - (b as i64).pow(5) == x {
+                println!("{} {}", a, b);
+                return;
+            }
+        }
+    }
 }
 
 #[allow(dead_code)]
