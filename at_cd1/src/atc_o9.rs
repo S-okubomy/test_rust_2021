f67de59940930cc3c308695fb9b35ffe8aa3d261
S-okubomy <okubo1504@gmail.com>
2022-05-31 21:53:53 +0900

    update

--- at_cd1/src/atc_o9.rs
+++ at_cd1/src/atc_o9.rs
@@ -1,3 +1,18 @@
+use proconio::{ input };
+
 fn main() {
-    println!("TODO");
+    square_inequality();
+}
+
+#[allow(dead_code)]
+fn square_inequality() {
+    input! {
+        a: usize, b: usize, c: usize,
+    }
+    
+    if a.pow(2) + b.pow(2) < c.pow(2) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
